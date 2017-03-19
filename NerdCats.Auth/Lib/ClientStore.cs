6e172c4262d0c6ce8f82a78ca4135dd16d966d05
Swagata Prateek <thehoneymad@hotmail.com>
2017-03-19 08:37:34 +0100

    Adding client store for MongoDb too

---
+++ NerdCats.Auth/Lib/ClientStore.cs
@@ -0,0 +1,24 @@
+﻿namespace NerdCats.Auth.Lib
+{
+    using System;
+    using System.Threading.Tasks;
+    using IdentityServer4.Models;
+
+    using IdentityServer4.Stores;
+    using NerdCats.Auth.Lib.Db;
+
+    public class ClientStore : IClientStore
+    {
+        private IDbContext dbContext;
+
+        public ClientStore(IDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public Task<Client> FindClientByIdAsync(string clientId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
